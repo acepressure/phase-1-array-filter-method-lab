716042d797d9421330eff435c09462002eb8bebb
Walter Simon <simonwalter986@gmail.com>
2022-06-21 13:22:40 +0300

    commit tree e82af872ff9f51d5d01d17a4a2b10d1b32581874

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(origin, find) {
+    return origin.filter((possibleMatch) => possibleMatch.toLowerCase() === find.toLowerCase());
+  }
+
+  function fuzzyMatch(origin, testString) {
+    return origin.filter((possibleMatch) => possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0);
+  }
+
+  function matchName(origin, findName) {
+    return origin.filter((info) => info.name === findName);
+  }
